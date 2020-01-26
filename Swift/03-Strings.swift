f5cd305ccfdf3099274fdd6b16d60da7cb653ff6
JavierSLX <jserranolule@gmail.com>
2020-01-26 10:59:02 -0600

    Completando Strings en Swift

--- Swift/03-Strings.swift
+++ Swift/03-Strings.swift
@@ -28,6 +28,36 @@ var myCaracter2 = "\u{24}"
 //Caracteres especiales (\n, \r, \t, \0, \')
 myString = "Bienvenidos al\n curso de Swift"
 
-//Emojis (Para agregar emojis directamente -> Ctrl + Cmd + Espacio
+//Emojis (Para agregar emojis directamente -> Ctrl + Cmd + Espacio)
 var myEmoji = "\u{1F496}"
 var myEmoji2 = "🧐"
+
+//Concatenacion
+myString = "Bienvenidos" + " al curso de Swift"
+myString += "\n¿Les está gustando?"
+
+//Interpolación (Colocando valores de una variable en un String)
+let name = "Javier"
+let country = "México"
+let myPersonalInfo = "Mi nombre es \(name) y vivo en \(country)"
+
+//Comparacion
+let name1 = "Javier"
+let name2 = "Juan"
+let name3 = "Javier"
+
+name1 == name2  //false
+name1 == name3  //true
+
+//Checa si el String contiene a otro
+let myContent = "Mi nombre es Javier"
+myContent.contains(name)    //true
+
+//Vacia
+myEmptyString.isEmpty   //true
+
+//Recorrido
+for valor in myContent
+{
+    print(valor)
+}
