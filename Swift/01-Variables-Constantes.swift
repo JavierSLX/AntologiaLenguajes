85b9bc053174995c3074e3baecac63058100655f
JavierSLX <jserranolule@gmail.com>
2020-01-25 19:54:59 -0600

    Comenzando con Swift

---
+++ Swift/01-Variables-Constantes.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//Variable (Se declaran de la siguiente manera)
+var str = "Hola mundo"
+var puntoCongelacionAgua = 0
+var cadena : String
+
+//Declaración múltiple
+var miNombreCompleto = "Jaier Serrano", miLugarDeNacimiento = "Celaya"
+
+//Constantes (Se declaran de la siguiente manera)
+let n = 2
+
+//Enteros
+var numero = 10
+var num : Int
+let m : Int = 9
+
+//Flotantes
+var flotante = 18.8
+var flot : Double
---
+++ Swift/02-OperadoresLogicos.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var numero = 10
+var flotante = 18.8
+
+//Operadores aritmeticos
+var sumaEnteros = numero + 2
+var sumaFlotantes = flotante + 2.2
+var suma = Double(numero) + flotante
+
+var resta = Int(flotante) - numero
+
+var multiplicacion = flotante * Double(numero)
+
+var division = Double(numero) / 3
