fc80d6ace4b83298651a5173fca0b8fa1ba76c7b
JavierSLX <jserranolule@gmail.com>
2020-01-26 10:44:43 -0600

    Strings

---
+++ Swift/03-Strings.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+//String en una linea
+var myString = "Bienvenido al curso de Swift"
+
+//String en varias lineas (Se deben de colocar 3 comillas al inicio y al final)
+var myMultipleString = """
+Bienvenidos al curso de Swift
+
+Espero que les parezca interezante el curso
+"""
+
+//String en una sola linea (Se deben de colocar 3 comillas al inicio y al final)
+var myFalseMultipleString = """
+Bienvenidos al curso de Swift\
+\
+Espero que les parezca interezante el curso
+"""
+
+//String vacio
+var myEmptyString = ""
+var myEmptyString2 = String()
+
+//Caracter usando Unicode
+var myCaracter = "\u{E9}"
+var myCaracter2 = "\u{24}"
+
+//Caracteres especiales (\n, \r, \t, \0, \')
+myString = "Bienvenidos al\n curso de Swift"
+
+//Emojis (Para agregar emojis directamente -> Ctrl + Cmd + Espacio
+var myEmoji = "\u{1F496}"
+var myEmoji2 = "🧐"
